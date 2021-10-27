bfdbb1498c7afb1a30ef814dedfa3ae9739550e7
Stefano Silvestri <stefano.silvestri89@gmail.com>
2021-10-27 17:11:27 +0200

    enable user to edit info + bug fixes

--- js/script.js
+++ js/script.js
@@ -13,7 +13,19 @@ import {
 let id = (doc, id) => doc.querySelector(id);
 
 let regForm = id(document, '#registerForm'),
-    loginForm = id(document, '#loginForm');
+    loginForm = id(document, '#loginForm'),
+    updateForm = id(document,'#updateForm');
+
+let bob = document.querySelectorAll('input:not(:required):not([type="submit"]):not([type="button"])');
+bob.forEach(function(inp){
+    inp.addEventListener('change',function(event){
+        if(this.value==''){
+            this.nextElementSibling==null ? '' : this.nextElementSibling.classList.remove('bob');
+        }else {
+            this.nextElementSibling==null ? '' : this.nextElementSibling.classList.add('bob');
+        };
+    })
+})
 
 /*============== 
     Sign Up 
@@ -44,6 +56,7 @@ regForm.addEventListener('submit', event1 => {
 
         userNew.save();
     }
+    regForm.reset();
 })
 
 
@@ -58,21 +71,29 @@ loginForm.addEventListener('submit', event2 => {
 
     let currentIndex = getIndex(username.value,password.value);
     if (currentIndex==-1){
-        id(loginForm,'#error').classList.remove('hidden');
+        Swal.fire({
+            icon:'error',
+            title: 'Login failed!',
+            text: 'You have entered incorrect credentials.'
+        })
     }else{
         id(loginForm,'#error').classList.add('hidden');
         loginForm.parentNode.classList.toggle('hidden');
         updateForm.parentNode.classList.toggle('hidden');
 
         let currentUser = getUser(currentIndex);
-        console.log(currentUser)
+        
         id(updateForm, "#updateUsername").value = currentUser.userName;
         id(updateForm, "#updateName").value = currentUser.name;
         id(updateForm, "#updateLastname").value = currentUser.lastName;
         id(updateForm, "#updateEmail").value = currentUser.email;
         id(updateForm, '#updateDob').value = currentUser.dob;
         id(updateForm, '#updatePhone').value = currentUser.phone;
-    }
+
+        id(updateForm, '#updateEmail').disabled = true;
+        sessionStorage.setItem('login',JSON.stringify(currentUser));
+    };
+    loginForm.reset();
 })
 
 
@@ -87,4 +108,48 @@ pulsanti.forEach(elem => {
         regForm.parentNode.classList.toggle('hidden');
         loginForm.parentNode.classList.toggle('hidden');
     })
+})
+
+updateForm.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    let tempUser = JSON.parse(sessionStorage.getItem('login'));
+    let currentUser = new User(tempUser.name, tempUser.userName, tempUser.email, tempUser.password);
+
+    currentUser.userName = id(updateForm, "#updateUsername").value;
+    currentUser.name = id(updateForm, "#updateName").value;
+    currentUser.lastName = id(updateForm, "#updateLastname").value;
+    currentUser.dob = id(updateForm, '#updateDob').value;
+    currentUser.phone = id(updateForm, '#updatePhone').value;
+    Swal.fire({
+        title:"Update your porfile?",
+        icon:'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, update!'
+    }).then((result)=>{
+        if(result.isConfirmed){
+            Swal.fire(
+                'Updated!',
+                'You will now be redirected to the login page',
+                'success'
+            );
+            let submitIndex = getIndex(currentUser.email, currentUser.password);
+            currentUser.update(submitIndex);
+            loginForm.parentNode.classList.toggle('hidden');
+            updateForm.parentNode.classList.toggle('hidden');
+            updateForm.reset();
+            sessionStorage.removeItem('login');
+        }
+    });
+});
+
+let logout = updateForm.querySelector('input[name="logout"]');
+logout.addEventListener('click', function(el){
+    el.preventDefault();
+    loginForm.parentNode.classList.toggle('hidden');
+    updateForm.parentNode.classList.toggle('hidden');
+    updateForm.reset();
+    sessionStorage.removeItem('login');
 })
