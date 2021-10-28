c2fe0d78392d7fb7b1a7655dd572e41c71aa52bb
Stefano Silvestri <stefano.silvestri89@gmail.com>
2021-10-28 18:06:22 +0200

    refactor class constructor and adjust scripts

--- js/profileScript.js
+++ js/profileScript.js
@@ -1,18 +1,14 @@
 import {
-    User, getUser
+  User,
+  getUser
 } from "./modules/User.js"
 import {
-    getIndex
+  getIndex
 } from "./modules/getIndex.js"
-import {
-    wrong,
-    wrongClear,
-    existsInStorage
-} from "./modules/wrong.js"
 
 let id = (doc, id) => doc.querySelector(id);
 
-let updateForm = id(document,'#updateForm');
+let updateForm = id(document, '#updateForm');
 
 
 /*================ 
@@ -30,116 +26,118 @@ id(updateForm, '#updatePhone').value = currentUser.phone;
 id(updateForm, '#updateEmail').disabled = true;
 */
 
-updateForm.addEventListener('submit',function(e){
-    e.preventDefault();
+updateForm.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    let tempUser = JSON.parse(sessionStorage.getItem('login'));
-    let currentUser = new User(tempUser.name, tempUser.userName, tempUser.email, tempUser.password);
+  let tempUser = JSON.parse(sessionStorage.getItem('login'));
+  let currentUser = new User(tempUser);
 
-    currentUser.userName = id(updateForm, "#updateUsername").value;
-    currentUser.name = id(updateForm, "#updateName").value;
-    currentUser.lastName = id(updateForm, "#updateLastname").value;
-    currentUser.dob = id(updateForm, '#updateDob').value;
-    currentUser.phone = id(updateForm, '#updatePhone').value;
-    Swal.fire({
-        title:"Update your porfile?",
-        icon:'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, update!'
-    }).then((result)=>{
-        if(result.isConfirmed){
-            Swal.fire(
-                'Updated!',
-                'You will now be redirected to the login page',
-                'success'
-            );
-            let submitIndex = getIndex(currentUser.email, currentUser.password);
-            currentUser.update(submitIndex);
-            updateForm.parentNode.classList.toggle('hidden');
-            updateForm.reset();
-            sessionStorage.removeItem('login');
-        }
-    });
+  currentUser.userName = id(updateForm, "#updateUsername").value;
+  currentUser.name = id(updateForm, "#updateName").value;
+  currentUser.lastName = id(updateForm, "#updateLastname").value;
+  currentUser.dob = id(updateForm, '#updateDob').value;
+  currentUser.phone = id(updateForm, '#updatePhone').value;
+  Swal.fire({
+    title: "Update your porfile?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, update!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Updated!',
+        'You will now be redirected to the login page',
+        'success'
+      );
+      let submitIndex = getIndex(currentUser.email, currentUser.password);
+      currentUser.update(submitIndex);
+      updateForm.parentNode.classList.toggle('hidden');
+      updateForm.reset();
+      sessionStorage.removeItem('login');
+    }
+  });
 });
 
 let logout = updateForm.querySelector('input[name="logout"]');
-logout.addEventListener('click', function(el){
-    el.preventDefault();
-    loginForm.parentNode.classList.toggle('hidden');
-    updateForm.parentNode.classList.toggle('hidden');
-    updateForm.reset();
-    sessionStorage.removeItem('login');
+logout.addEventListener('click', function (el) {
+  el.preventDefault();
+  sessionStorage.removeItem('login');
 })
 
 
 /*=============
     Calendar
 ===============*/
-
 /*
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      initialDate: '2021-10-07',
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+let cal = document.querySelector('input[name="calendar"]')
+
+cal.addEventListener('click', function () {
+  var calendarEl = document.getElementById('calendar');
+  calendarEl.classList.toggle('hidden');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    initialDate: '2021-10-07',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    selectable: true,
+    select: function(info) {
+      alert('selected ' + info.startStr + ' to ' + info.endStr);
+    },
+    events: [{
+        title: 'All Day Event',
+        start: '2021-10-01'
       },
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2021-10-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2021-10-07',
-          end: '2021-10-10'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2021-10-09T16:00:00'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2021-10-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2021-10-11',
-          end: '2021-10-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2021-10-12T10:30:00',
-          end: '2021-10-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2021-10-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2021-10-12T14:30:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2021-10-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2021-10-28'
-        }
-      ]
-    });
-  
-    calendar.render();
+      {
+        title: 'Long Event',
+        start: '2021-10-07',
+        end: '2021-10-10'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        start: '2021-10-09T16:00:00'
+      },
+      {
+        groupId: '999',
+        title: 'Repeating Event',
+        start: '2021-10-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2021-10-11',
+        end: '2021-10-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2021-10-12T10:30:00',
+        end: '2021-10-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2021-10-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2021-10-12T14:30:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2021-10-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2021-10-28'
+      }
+    ]
   });
-  */
+
+  calendar.render();
+});
+*/
